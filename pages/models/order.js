3300cb7822949825d57b2660cdd4a49b27ff04ac
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2023-03-03 00:55:21 +0530

    pl

--- pages/models/order.js
+++ pages/models/order.js
@@ -8,7 +8,7 @@ const OrderSchema = new Schema({
         ProductId: {type: String},
         quantity: {type: Number, default:1}
     }],
-    address: {type:string,required:true},
+    address: {type:String,required:true},
     amount: { type: Number, required:true },
     status: { type: String, default: 'Pending',required: true }
     
