e29000d99837b7380c349ca019583a6b03fdcf11
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2023-03-03 11:42:06 +0530

    ju

--- pages/t-shirts.js
+++ pages/t-shirts.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import product from "./models/product"
+import product from "./../models/product"
 const mongoose = require('mongoose');
 const Tshirts = ({products}) => {
   
