1aad62d9ede7db5bd44cea6a75f7a3ed15990ddb
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2023-03-03 11:53:54 +0530

    wd

--- pages/api/getproduct.js
+++ pages/api/getproduct.js
@@ -1,4 +1,4 @@
-import product from "../models/product"
+import product from "../../models/product"
 import connectDb from "../../middleware/mongoose"
 
 
--- pages/product/[Slug].js
+++ pages/product/[Slug].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router'
 
 import { useState,useEffect } from 'react'
-import product from '../models/product'
+import product from '../../models/product'
 import mongoose from 'mongoose'
 
 
