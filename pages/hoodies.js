1ac69d49858cb4049938c1302cd6d48d4a0def44
anshugarg401 <92512766+anshugarg401@users.noreply.github.com>
2023-03-10 17:49:53 +0530

    j

--- pages/hoodies.js
+++ pages/hoodies.js
@@ -33,7 +33,7 @@ const Hoodies = ({products}) => {
                     </h>
                     <p className="mt-1">{k.price}</p>
                     
-                    {/* <div className="flex mx-2">
+                    <div className="flex mx-2">
                     {products[k].colour.includes('yellow') && <button className="mx-1 border-2 border-gray-300 ml-1 bg-yellow-300 rounded-full w-5 h-5 focus:outline-none"></button>}  
                     {products[k].colour.includes('red') && <button className="mx-1 border-2 border-gray-300 ml-1 bg-red-600 rounded-full w-5 h-5 focus:outline-none"></button>}  
                     {products[k].colour.includes('green') && <button className="mx-1 border-2 border-gray-300 ml-1 bg-green-600 rounded-full w-5 h-5 focus:outline-none"></button>}  
@@ -45,7 +45,7 @@ const Hoodies = ({products}) => {
                      {products[k].size.includes('L') && <span className="border-2 rounded-md mx-1 p-1" >L</span>}  
                      {products[k].size.includes('M') && <span className="border-2 rounded-md mx-1 p-1" >M</span>}  
                      {products[k].size.includes('S') && <span className="border-2 rounded-md mx-1 p-1" >S</span>} 
-                    </div> */}
+                    </div>
                   </div>
                 </div>
               </Link>
